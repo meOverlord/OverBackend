5169821818b5de99ec275cb18531b6f80fe5db23
Benjamin Herbomez <benjamin.herbomez@gmail.com>
2020-02-19 21:29:27 +0100

    create first modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+import { ClientModule } from './client/client.module';
+import { appConfig, mongoConfig } from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [appConfig, mongoConfig]
+        }),
+        AuthModule, ClientModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
