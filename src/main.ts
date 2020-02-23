71883a923ed56429068c3147b0b7cb34ea5bd154
Kamule <benjamin.herbomez@gmail.com>
2020-02-23 18:55:44 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT | 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
