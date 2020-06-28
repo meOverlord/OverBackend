8f30e7c475d7c14df7a2ee4543c5d52517b18e1e
Kamule <benjamin.herbomez@gmail.com>
2020-06-28 15:28:11 +0200

    customize client collection

--- api/clients/controllers/clients.js
+++ api/clients/controllers/clients.js
@@ -3,6 +3,27 @@
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
+ * 
+ * https://strapi.io/documentation/3.0.0-beta.x/guides/is-owner.html#limit-the-update
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.owner = ctx.state.user.id;
+      entity = await strapi.services.clients.create(data, { files });
+    } else {
+      ctx.request.body.owner = ctx.state.user.id;
+      entity = await strapi.services.clients.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+};
